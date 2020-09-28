14bd99d902fc7effea1ad3337639c8478cdd4792
Gaetan Manchon <gmanchon@gmail.com>
2020-09-28 10:49:01 +0200

    moved notebook, added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+REQUIRED_PACKAGES = [
+    'pandas==0.24.2']
+
+setup(
+    name='WagonIntroToDS',
+    version='1.0',
+    install_requires=REQUIRED_PACKAGES,
+    description='Wagon Intro to Data Science'
+)
